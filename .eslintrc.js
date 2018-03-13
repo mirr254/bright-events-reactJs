ca2713e12737429c97fb178b4842794b67f11b7c
samuel ngigi <shammir@Tonie-MacBook-Pro.local>
2018-03-13 22:31:23 +0300

    [CHORE 155855754] Adds travis.yml in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,34 @@
 module.exports = {
-    //"extends": "airbnb-base",
+    "extends": "airbnb",
     "root": true,
+    //"extends": "airbnb-base",
     "env": {
         "node": true,
         "es6": true,
         "mocha": true
     },
-    parser: "babel-eslint",
-    "plugins": [
-        "react"
-    ],
     "rules": {
-        // "react/prop-types": [2],
-        // "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        // "max-len": [1, 120, 2, {ignoreComments: true}],
-        // "linebreak-style": 0,     
+        "one-var": 0,
+        "one-var-declaration-per-line": 0,
+        "new-cap": 0,
+        "consistent-return": 0,
+        "no-param-reassign": 0,
+        "comma-dangle": 0,
+        "curly": ["error", "multi-line"],
+        "import/no-unresolved": [2, { commonjs: true }],
+        "no-shadow": ["error", { "allow": ["req", "res", "err"] }],
+        "valid-jsdoc": ["error", {
+            "requireReturn": true,
+            "requireReturnType": true,
+            "requireParamDescription": false,
+            "requireReturnDescription": true
+        }],
+        "require-jsdoc": ["error", {
+            "require": {
+                "FunctionDeclaration": true,
+                "MethodDefinition": true,
+                "ClassDeclaration": true
+            }
+        }]
     }
 };
-
