599615deffae2d660b919507905d2f3b6cb218cc
samuel ngigi <shammir@Tonie-MacBook-Pro.local>
2018-03-20 01:24:34 +0300

    [CHORE 156075846] Sets up testing entrypoint script

--- src/TestSetup.js
+++ src/TestSetup.js
@@ -6,3 +6,24 @@ require('babel-register');
 
 // disable webpack specific features since mocha doesn't know what to do with them
 require.extensions['.css'] = function () { return null;};
+require.extensions['.jpg'] = function () { return null; };
+require.extensions['.png'] = function () { return null; };
+
+//set up jsdom for virtual inmemory dom
+var jsdom = require('jsdom').jsdom;
+// set up global variables that help simulate browser enviroment
+var exposedProperties = ['window','document','navigator'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach( (property) =>{
+    if (typeof global[property] === 'undefined'){
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
+});
+
+global.navigator = {
+    userAgent: 'node.js'
+};
+
