bdccecb9ec770e40a5fe08fd0d9624983b89fada
samuel ngigi <shammir@Tonie-MacBook-Pro.local>
2018-04-17 00:37:55 +0300

    [Fix #155855974] Fixes login functional component

--- src/pages/LoginComponent.js
+++ src/pages/LoginComponent.js
@@ -1,104 +1,46 @@
-import React, { Component } from "react";
-import { Jumbotron } from 'reactstrap';
-import LoginForm from '../components/LoginFormComponent';
-import { LoginFormErrors } from '../components/FormErros';
-import "../assets/login.css";
-import axios from "axios";
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-export default class Login extends Component {
-    constructor(props){
-        //passing props to the parent class
+class Login extends Component {
+    constructor(props) {
         super(props);
-        //set initial state
         this.state = {
-            data : [],
             username: '',
-            password: '',
-            loginFormErrors: {username: '',password:'' },
-            usernameValid: false,
-            passwordValid: false,
-            formValid: false
-        };
-
-        // this.LOGIN_API_URL = 'https://brighter-event.herokuapp.com/api/v1/auth/login';
-    }
-
-    //lifecycle method
-    componentDidMount(){
-        //make a get request
-        axios({
-            method: 'get',
-            url: this.LOGIN_API_URL,
-        })
-            .then(function (response) {
-                console.log(response)
-            });
-
-    }
-
-    //login user handler
-    onChange = (event) => { this.handleUserInput(event) }
-
-    handleUserInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({[name]: value}, 
-        ()=> {this.validateFormFields(name,value) });
-    }
-
-    validateFormFields(fieldName, value){
-        let validationErrors = this.state.loginFormErrors;
-        let usernameValid = this.state.usernameValid;
-        let passwordValid = this.state.passwordValid
-
-        switch (fieldName) {
-            case 'username':
-                usernameValid = value.length > 4;
-                validationErrors.username = usernameValid ? "" : " is too short";
-                break;
-
-            case 'password':
-                passwordValid = value.length >= 7 && value.match(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/);
-                validationErrors.password = passwordValid ? "" : "password must have atleast 6 characters. both letters numbers";
-                break;
-        
-            default:
-                break;
+            password: ''
         }
-
-        this.setState({
-            loginFormErrors : validationErrors,
-            usernameValid : usernameValid,
-            passwordValid : passwordValid
-        }, this.validateForm);
     }
-
-    // a function to set the state of a valid or invalid form
-    validateForm(){
-        this.setState({
-            formValid : this.state.usernameValid && this.state.passwordValid
-        });
-    }
-
-    handleLogin = () => {
-        console.log(this.state)
-    }
-        
     render() {
         return (
             <div>
-                <Jumbotron>
-                <div className="login">
-                    <div className="panel panel-default">
-                        <LoginFormErrors loginFormErrors={this.state.loginFormErrors} />
+                <MuiThemeProvider>
+                    <div>
+                        <AppBar
+                            title="Login"
+                        />
+                        <TextField
+                            hintText="Enter your Username"
+                            floatingLabelText="Username"
+                            onChange={(event, newValue) => this.setState({ username: newValue })}
+                        />
+                        <br />
+                        <TextField
+                            type="password"
+                            hintText="Enter your Password"
+                            floatingLabelText="Password"
+                            onChange={(event, newValue) => this.setState({ password: newValue })}
+                        />
+                        <br />
+                        <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)} />
                     </div>
-                    <LoginForm
-                            onChange={this.onChange}
-                            handleLogin={this.handleLogin}
-                     />
-                </div>
-                </Jumbotron>
+                </MuiThemeProvider>
             </div>
         );
     }
 }
+const style = {
+    margin: 15,
+};
+export default Login;
