605d8f6a5a7119e025626198d7f139341a6617c4
samuel ngigi <shammir@Tonie-MacBook-Pro.local>
2018-04-18 01:03:55 +0300

    [Fix #155855873 ] Routing errors in App.js file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
-import React, { Component } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import './assets/App.css';
-import Loginscreen from './pages/LoginScreen';
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+import Routes from "./Routes";
+import React, { Component } from "react";
+import "./assets/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loginPage: [],
-            uploadScreen: []
-        }
-    }
-    componentWillMount() {
-        var loginPage = [];
-        loginPage.push(<Loginscreen parentContext={this} />);
-        this.setState({
-            loginPage: loginPage
-        })
-    }
     render() {
         return (
-            <div className="App">
-                {this.state.loginPage}
-                {/* {this.state.uploadScreen} */}
+            <div className="App container">
+                <Routes />
             </div>
         );
     }
 }
-const style = {
-    margin: 15,
-};
+
 export default App;
