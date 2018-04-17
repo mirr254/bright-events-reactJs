46491c87ebf868f32914a469d3ac72585e68e3cb
samuel ngigi <shammir@Tonie-MacBook-Pro.local>
2018-04-17 15:55:45 +0300

    [Chore #155855873] Removes the commented code

--- src/pages/LoginComponent.js
+++ src/pages/LoginComponent.js
@@ -16,18 +16,15 @@ class Login extends Component {
 
     }
     
-
     handleClick(event) {
         var apiBaseUrl = "https://brighter-event.herokuapp.com/api/v1/auth/";
-        var self = this;
-        var payload = {
-            "email": this.state.username,
-            "password": this.state.password
-        }
+        
         const requestPromise = require('request-promise');
+        // encode data with base64 for the authentication
         const base64encodedData = new Buffer(
             this.state.username + ':' + this.state.password
         ).toString('base64');
+        
         requestPromise.get({
             uri: apiBaseUrl+"login",
             headers: {
@@ -39,27 +36,7 @@ class Login extends Component {
         }).catch(function fail(error) {
             console.log(error);
         }) ;
-        // axios.post(apiBaseUrl + 'login', payload)
-        //     .then(function (response) {
-        //         console.log(response);
-        //         if (response.data.code === 200) {
-        //             console.log("Login successfull");
-        //             var uploadScreen = [];
-        //             //uploadScreen.push(<UploadScreen appContext={self.props.appContext} />)
-        //             //self.props.appContext.setState({ loginPage: [], uploadScreen: uploadScreen })
-        //         }
-        //         else if (response.data.code === 204) {
-        //             console.log("Username password do not match");
-        //             alert("username password do not match")
-        //         }
-        //         else {
-        //             console.log("Username does not exists");
-        //             alert("Username does not exist");
-        //         }
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
+        
     }
 
     render() {
